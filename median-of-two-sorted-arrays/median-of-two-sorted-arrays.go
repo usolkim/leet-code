314279c035ffbff7c88865dbd1fb99a16b4f9833
usolkim <plpot@naver.com>
2018-07-03 00:16:24 +0900

    accepted

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -21,46 +21,48 @@ func main() {
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	l1 := len(nums1)
-	l2 := len(nums2)
-	if l1 > l2 {
-		nt := nums1
-		nums2 = nums1
-		nums1 = nt
-		lt := l1
-		l1 = l2
-		l2 = lt
+	len1 := len(nums1)
+	len2 := len(nums2)
+	if len1 > len2 {
+		nums_t := nums1
+		nums1 = nums2
+		nums2 = nums_t
+		len_t := len1
+		len1 = len2
+		len2 = len_t
 	}
 
-	var s, e, m2d int = 0, l1, int((l1 + l2 + 1) / 2)
+	var start, end, m2d int = 0, len1, int((len1 + len2 + 1) / 2)
 	var isOdd bool
 	var res float64
-	if (l1+l2)%2 == 0 {
+	if (len1+len2)%2 == 0 {
 		isOdd = true
 	}
 
-	for i := 0; i < 10; i++ {
-		m1 := int((s + e) / 2)
+	for start <= end {
+		m1 := int((start + end) / 2)
 		m2 := m2d - m1
 
-		fmt.Println(m1, m2)
-
-		if nums2[m2-1] > nums1[m1] {
-			if m1 + 1 == l1 {
-				s = l1
-			} else {
-				s = m1
-			}
-		} else if nums1[m1-1] > nums2[m2] {
-			e = m1 + 1
+		if m1 < len1 && nums2[m2-1] > nums1[m1] {
+			start = m1 + 1
+		} else if m1 > 0 && nums1[m1-1] > nums2[m2] {
+			end = m1 - 1
 		} else {
-			if nums1[m1-1] < nums2[m2-1] {
+			if m1 == 0 {
+				res = float64(nums2[m2-1])
+			} else if m2 == 0 {
+				res = float64(nums1[m1-1])
+			} else if nums1[m1-1] < nums2[m2-1] {
 				res = float64(nums2[m2-1])
 			} else {
 				res = float64(nums1[m1-1])
 			}
 			if isOdd {
-				if nums1[m1] < nums2[m2] {
+				if m1 == len1 {
+					res = (res + float64(nums2[m2])) / 2
+				} else if m2 == len2 {
+					res = (res + float64(nums1[m1])) / 2
+				} else if nums1[m1] < nums2[m2] {
 					res = (res + float64(nums1[m1])) / 2
 				} else {
 					res = (res + float64(nums2[m2])) / 2
