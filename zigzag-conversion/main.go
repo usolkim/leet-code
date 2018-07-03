9de796c289dc6cc0a767a51dd8fb51a257aeece4
usolkim <plpot@naver.com>
2018-07-03 23:25:44 +0900

    accepted!

--- zigzag-conversion/main.go
+++ zigzag-conversion/main.go
@@ -2,30 +2,38 @@ package main
 
 import (
   "fmt"
-  "strings"
 )
 
-func main(){
+func main() {
   fmt.Println(convert("PAYPALISHIRING", 3))
   fmt.Println(convert("PAYPALISHIRING", 4))
 }
 
 func convert(s string, numRows int) string {
-    if numRows == 1 {
-      return s
-    }
-    ss := []string{}
-    for i:=1; i<=numRows; i++ {
-      k := i-1
-      ss = append(ss, string(s[k]))
-      for {
-        k = k + 2*(numRows-i) - 1
-        if k >= len(s) {
-          break;
-        }
-        fmt.Println(numRows, i, k, len(s))
-        ss = append(ss, string(s[k]))
+  if numRows == 1 {
+    return s
+  }
+  s_len := len(s)
+  res := make([]byte, s_len)
+  k := 2 * (numRows - 1)
+  ii := 0
+  for i := 0; i < numRows && i < s_len; i++ {
+
+    if i == 0 || i == numRows-1 {
+      for j := i; j < s_len; j += k {
+        res[ii] = s[j]
+        ii++
+      }
+    } else {
+      res[ii] = s[i]
+      ii++
+      l := k - (2 * i)
+      for j := i + l; j < s_len; j += l {
+        res[ii] = s[j]
+        ii++
+        l = k - l
       }
     }
-    return strings.Join(ss, "")
+  }
+  return string(res)
 }
