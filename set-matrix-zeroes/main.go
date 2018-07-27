df5b9220d399ff086bb246f0fe368d288b50c84b
usol.kim <plpot@naver.com>
2018-07-27 22:57:55 +0900

    accepted

--- set-matrix-zeroes/main.go
+++ set-matrix-zeroes/main.go
@@ -30,8 +30,31 @@ func main() {
 	print(matrix)
 
 	matrix = make([][]int, 2)
-	matrix[0] = []int{5,4,4,3,9,2,6,0,9,0}
-	matrix[1] = []int{5,4,4,3,1,5,2,6,5,6}
+	matrix[0] = []int{5, 4, 4, 3, 9, 2, 6, 0, 9, 0}
+	matrix[1] = []int{5, 4, 4, 3, 1, 5, 2, 6, 5, 6}
+	print(matrix)
+	setZeroes(matrix)
+	print(matrix)
+
+	matrix = make([][]int, 5)
+	matrix[0] = []int{8, 3, 1, 4, 6, 4, 0, 3, 4}
+	matrix[1] = []int{9, 1, 3, 0, 1, 5, 7, 4, 1}
+	matrix[2] = []int{2, 2, 5, 2147483647, 5, 4, 4, 3, 8}
+	matrix[3] = []int{4, 9, 7, 0, 3, 6, 9, 5, 9}
+	matrix[4] = []int{4, 1, 8, 8, 4, 1, 5, 7, 6}
+	print(matrix)
+	setZeroes(matrix)
+	print(matrix)
+
+	matrix = make([][]int, 8)
+	matrix[0] = []int{3, 5, 5, 6, 9, 1, 4, 5, 0, 5}
+	matrix[1] = []int{2, 7, 9, 5, 9, 5, 4, 9, 6, 8}
+	matrix[2] = []int{6, 0, 7, 8, 1, 0, 1, 6, 8, 1}
+	matrix[3] = []int{7, 2, 6, 5, 8, 5, 6, 5, 0, 6}
+	matrix[4] = []int{2, 3, 3, 1, 0, 4, 6, 5, 3, 5}
+	matrix[5] = []int{5, 9, 7, 3, 8, 8, 5, 1, 4, 3}
+	matrix[6] = []int{2, 4, 7, 9, 9, 8, 4, 7, 3, 7}
+	matrix[7] = []int{3, 5, 2, 8, 8, 2, 2, 4, 9, 8}
 	print(matrix)
 	setZeroes(matrix)
 	print(matrix)
@@ -55,36 +78,69 @@ func setZeroes(matrix [][]int) {
 		return
 	}
 
-	row, col := 0, 0
+	frow := false
+	fcol := false
 
-	mp := 1<<uint(m+2) - 2
-	np := 1<<uint(n+2) - 2
+	if matrix[0][0] == 0 {
+		fcol = true
+		frow = true
+	}
 
-	for i := 0; i < m; i++ {
-		row = (row << 1) + 1
-		for j := 0; j < n; j++ {
-			if matrix[i][j] == 0 {
-				row &= np
+	if !fcol {
+		for i := 1; i < m; i++ {
+			if matrix[i][0] == 0 {
+				fcol = true
+				break
 			}
 		}
 	}
 
-	for j := 0; j < n; j++ {
-		col = (col << 1) + 1
-		for i := 0; i < m; i++ {
-			if matrix[i][j] == 0 {
-				col &= mp
+	if !frow {
+		for j := 1; j < n; j++ {
+			if matrix[0][j] == 0 {
+				frow = true
+				break
 			}
 		}
 	}
 
-	for i := 0; i < m; i++ {
-		rtmp := 1 << uint(m-i-1)
-		for j := 0; j < n; j++ {
-			ctmp := 1 << uint(n-j-1)
-			if row&rtmp == 0 || col&ctmp == 0 {
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+
+	// print(matrix)
+
+	for i := 1; i < m; i++ {
+		if matrix[i][0] == 0 {
+			for j := 1; j < n; j++ {
 				matrix[i][j] = 0
 			}
+		}
+	}
+
+	for j := 1; j < n; j++ {
+		if matrix[0][j] == 0 {
+			for i := 1; i < m; i++ {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	if fcol {
+		matrix[0][0] = 0
+		for i := 1; i < m; i++ {
+			matrix[i][0] = 0
+		}
+	}
+	if frow {
+		matrix[0][0] = 0
+		for j := 1; j < n; j++ {
+			matrix[0][j] = 0
 		}
 	}
 }
